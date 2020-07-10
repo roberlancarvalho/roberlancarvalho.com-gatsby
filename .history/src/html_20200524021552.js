7ca59f7d342ea0d55dd8a6e088eccea91a3621e9
roberlan <roberlan.carvalho@gmail.com>
2020-07-10 00:25:14 -0300

    Enviando novas alterações

--- .history/src/html_20200524021552.js
+++ .history/src/html_20200524021552.js
@@ -76,8 +76,8 @@ export default function HTML(props) {
 HTML.propTypes = {
   htmlAttributes: PropTypes.object,
   headComponents: PropTypes.array,
-  preBodyComponents: PropTypes.array,
   bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
   body: PropTypes.string,
   postBodyComponents: PropTypes.array,
 }
