f13154926dd6165fb8fb022492716f315b7a4ed9
roberlan <roberlan.carvalho@gmail.com>
2020-07-10 13:30:44 -0300

    Alterações

--- .history/gatsby-config_20200710005319.js
+++ .history/gatsby-config_20200710005319.js
@@ -84,9 +84,9 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-netlify-cms`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-offline`
   ],
 }
